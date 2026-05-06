dc017fba6ae25658d94e48d2af04446cef9e4587
Arshia Bourbour <140833200+ArshiaBourbour@users.noreply.github.com>
2026-05-06 22:19:10 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-# config.py
-TOKEN = "Your Bot token"
+TOKEN = "Your bot token" ## get it from @botfather
 BASE_URL = f"https://tapi.bale.ai/bot{TOKEN}"
 
-ADMIN_USERNAME = "cl_admin10"
-ADMIN_PASSWORD = "qweASD10"
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "pswrd"
 
 MAX_ADMIN_ATTEMPTS = 3
 ADMIN_BLOCK_MINUTES = 10
